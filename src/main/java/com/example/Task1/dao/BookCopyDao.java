ac0601836d3cdcea5db1aeb88c90ea945484386a
kksmn <71097529+kksmn@users.noreply.github.com>
2022-01-25 13:34:18 +0300

    Update BookCopyDao.java

--- src/main/java/com/example/Task1/dao/BookCopyDao.java
+++ src/main/java/com/example/Task1/dao/BookCopyDao.java
@@ -3,8 +3,8 @@ package com.example.Task1.dao;
 import com.example.Task1.models.BookCopy;
 
 public interface BookCopyDao {
-    boolean isBookAvailable(Long id);
-    void addNewCopy(Long bookId,Double price, Double priceForDay);
+    void addNewCopy(Long bookId, Double price, Double priceForDay);
+
     void makeBookAvailable(Long id);
 
 }
