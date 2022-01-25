bb32e0169fbdccc9bb7d80bc38089ace25fcab32
kksmn <71097529+kksmn@users.noreply.github.com>
2022-01-25 13:33:33 +0300

    Update AuthorDao.java

--- src/main/java/com/example/Task1/dao/AuthorDao.java
+++ src/main/java/com/example/Task1/dao/AuthorDao.java
@@ -5,13 +5,12 @@ import com.example.Task1.models.Author;
 import java.util.List;
 
 public interface AuthorDao {
-    List<Long> findBookIdByAuthor(Long id);
-    List<Long> findAuthorIdByBook(Long id);
-    Long addNewAuthor(String name,String path);
+    Long addNewAuthor(String name, String path);
+
     Long getAuthorByName(String name);
-    Author getAuthorById(Long id);
+
     void addAuthorId(Long bookId, Long authorId);
-    List<Author> addAuthorToList(String[] authors, String[] authorImage);
-    void addAuthorToBook(Long bookId,List<Long> authors);
+
+    void addAuthorToBook(Long bookId, List<Long> authors);
 
 }
